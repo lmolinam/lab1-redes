c35dbe12841358ae63280317d5da88c6b03af8eb
lmolinam <luis.molina@usach.cl>
2020-07-05 22:32:38 -0400

    fix: change files path manipulation

--- src/modulacion-pcm.py
+++ src/modulacion-pcm.py
@@ -3,10 +3,16 @@ import struct
 import sys
 import wave
 
+from os.path import dirname, join, abspath
+
+PATH_WAV = join(dirname(dirname(abspath(__file__))), "wav")
+PATH_TXT = join(dirname(dirname(abspath(__file__))), "txt")
 ARCHIVO_SALIDA = 'codificación-enviada.txt'
 
+#print(f"PATH_WAV: {PATH_WAV}")
+
 def obtener_muestras_wav(nombre_wav):
-    with wave.open(nombre_wav, 'r') as wav_entrada:
+    with wave.open(join(PATH_WAV,nombre_wav), 'r') as wav_entrada:
         frames = wav_entrada.getnframes()
         data = wav_entrada.readframes(frames)
         data = struct.unpack('{n}h'.format(n=frames), data)
@@ -19,7 +25,7 @@ def digitalizar_muestras(muestras_disc):
     return [np.binary_repr(muestra) for muestra in muestras_disc]
 
 def escribir_codificacion(muestras_dig):
-    with open(ARCHIVO_SALIDA, 'w') as txt_file:
+    with open(join(PATH_TXT, ARCHIVO_SALIDA), 'w') as txt_file:
         txt_file.write(repr(muestras_dig))
 
 
